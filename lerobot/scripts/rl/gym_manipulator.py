6daeb2c4f43a15eff30a0c7dd5b4f2d48cd5fdd9
jiangziben <jiangziben@foxmail.com>
2025-07-22 15:48:18 +0800

    Fix:1.增加不采集数据也可以可视化图像和力 2.增加姿态导纳控制

--- lerobot/scripts/rl/gym_manipulator.py
+++ lerobot/scripts/rl/gym_manipulator.py
@@ -2082,7 +2082,7 @@ def record_dataset(env, policy, cfg):
     recorded_action = None
     fig, axes =plt.subplots(2,1)
     fig.set_size_inches(25,50)
-    # vis = RealTimeForceVisualizer()
+    vis = RealTimeForceVisualizer()
     while episode_index < cfg.num_episodes:
         obs, _ = env.reset()
         start_episode_t = time.perf_counter()
@@ -2110,9 +2110,10 @@ def record_dataset(env, policy, cfg):
             axes[1].axis("off")
             axes[1].imshow(obs["observation.images.wrist"][0].permute(1,2,0).cpu().numpy())
             plt.pause(0.01)
-            # # show force
-            # force = obs["observation.state"][0][19:22].cpu().numpy()
-            # vis.update_force(force)
+            # show force
+            force = obs["observation.state"][0][19:22].cpu().numpy()
+            print("force:", force)
+            vis.update_force(force)
 
             # Check if episode needs to be rerecorded
             if info.get("rerecord_episode", False):
@@ -2260,6 +2261,9 @@ def main(cfg: EnvConfig):
 
     num_episode = 0
     successes = []
+    fig, axes =plt.subplots(2,1)
+    fig.set_size_inches(25,50)
+    vis = RealTimeForceVisualizer()
     while num_episode < 10:
         start_loop_s = time.perf_counter()
         # Sample a new random action from the robot's action space.
@@ -2269,6 +2273,18 @@ def main(cfg: EnvConfig):
 
         # Execute the step: wrap the NumPy action in a torch tensor.
         obs, reward, terminated, truncated, info = env.step(smoothed_action)
+        #show the current observation
+        axes[0].clear()
+        axes[0].axis("off")
+        axes[0].imshow(obs["observation.images.front"][0].permute(1,2,0).cpu().numpy())
+        axes[1].clear()
+        axes[1].axis("off")
+        axes[1].imshow(obs["observation.images.wrist"][0].permute(1,2,0).cpu().numpy())
+        plt.pause(0.01)
+        # show force
+        force = obs["observation.state"][0][19:22].cpu().numpy()
+        print("force:", force)
+        vis.update_force(force)
         if terminated or truncated:
             successes.append(reward)
             env.reset()
